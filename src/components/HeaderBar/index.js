986bdfdc7399f94b7be2b5f8b132676d1fe142d5
Daazzer <w513765485@163.com>
2022-10-16 21:08:37 +0800

    头部状态栏样式更改

--- src/components/HeaderBar/index.js
+++ src/components/HeaderBar/index.js
@@ -1,27 +1,59 @@
 import { createElement } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Layout } from 'antd';
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Link, useHistory } from 'react-router-dom';
+import { Avatar, Dropdown, Layout, Menu } from 'antd';
+import { MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined } from '@ant-design/icons';
 import style from './index.module.scss';
 
 const { Header } = Layout;
 
 function HeaderBar() {
   const dispatch = useDispatch();
+  const history = useHistory();
+  const { user } = useSelector(state => state.login);
   const { collapsed } = useSelector(state => state.style);
   const handleTriggerClick = () => dispatch({ type: 'style/TOGGLE_COLLAPSED' });
+  const handleLogout = () => {
+    dispatch({ type: 'login/LOGOUT' });
+    history.replace('/login');
+  };
+
+  const menu = (
+    <Menu
+      items={[
+        {
+          label: <Link to="/news">游客系统</Link>,
+          key: '0'
+        },
+        {
+          label: '退出',
+          danger: true,
+          key: '1',
+          onClick: handleLogout
+        }
+      ]}
+    />
+  );
 
   return (
-    <Header
-      className={style.headerBar}
-      style={{
-        padding: 0
-      }}
-    >
+    <Header className={style.headerBar}>
       {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
         className: style.siderBarTrigger,
         onClick: handleTriggerClick,
       })}
+      <div className={style.infoBar}>
+        <div className="info-text">欢迎<span className={style.username}>{user?.username || 'unknown'}</span></div>
+        <Dropdown
+          trigger={['click']}
+          overlay={menu}
+        >
+          <Avatar
+            className={[style.avatar, 'info-text'].join(' ')}
+            size="large"
+            icon={<UserOutlined />}
+          />
+        </Dropdown>
+      </div>
     </Header>
   );
 }
--- src/views/Main/Home.js
+++ src/views/Main/Home.js
@@ -1,19 +1,7 @@
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { Button } from 'antd';
-
 function Home() {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const handleLogout = () => {
-    dispatch({ type: 'login/LOGOUT' });
-    history.replace('/login');
-  };
-
   return (
     <div className="home">
       Home
-      <Button type="primary" onClick={handleLogout}>退出</Button>
     </div>
   );
 }
