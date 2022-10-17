379e261317a345d45438c7b6467f6f8c14d5902a
Daazzer <w513765485@163.com>
2022-10-17 12:37:29 +0800

    设置侧边栏数据

--- src/components/SiderBar/index.js
+++ src/components/SiderBar/index.js
@@ -1,4 +1,5 @@
-import { NavLink } from 'react-router-dom';
+import { useEffect, useState, useCallback } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import {
   Layout,
@@ -6,18 +7,118 @@ import {
   Tooltip
 } from 'antd';
 import {
-  UploadOutlined,
+  HomeOutlined,
   UserOutlined,
-  VideoCameraOutlined
+  KeyOutlined,
+  ProfileOutlined,
+  AuditOutlined,
+  CommentOutlined
 } from '@ant-design/icons';
+import { getPermissions } from '@/api/user';
+import { getAssembleTree } from '@/utils';
 import logo from '@/assets/logo.svg';
 import style from './index.module.scss';
 
 const { Sider } = Layout;
 
+const iconMap = {
+  home: <HomeOutlined />,
+  'user-manage': <UserOutlined />,
+  'permission-manage': <KeyOutlined />,
+  'news-manage': <ProfileOutlined />,
+  'audit-manage': <AuditOutlined />,
+  'publish-manage': <CommentOutlined />
+};
+
+const getKeys = permissions => {
+  // eslint-disable-next-line
+  let currentLevelFirstNode = permissions.find(menu => menu.parentId === 0);
+  let key = String(currentLevelFirstNode.id);
+  let defaultOpenKeys = [];
+  let defaultSelectedKeys = [];
+
+  defaultOpenKeys.push(key);
+
+  while (currentLevelFirstNode) {
+    // eslint-disable-next-line
+    currentLevelFirstNode = permissions.find(menu => menu.parentId === currentLevelFirstNode.id);
+    key = String(currentLevelFirstNode?.id || '');
+
+    if (key) {
+      defaultOpenKeys.push(key);
+    }
+  }
+
+  defaultSelectedKeys = defaultOpenKeys.filter((_, index) => index === defaultOpenKeys.length - 1);
+
+  return {
+    defaultOpenKeys,
+    defaultSelectedKeys
+  };
+};
+
+const getMenus = permissions => {
+  const fn = (permissions, paths = []) => permissions.map(permission => {
+    const { children, id, parentId, key, name: label } = permission;
+    const menu = {
+      key: String(id),
+      label,
+      icon: iconMap[key]
+    };
+
+    if (!parentId) {
+      paths = [];
+    }
+
+    paths.push(key);
+
+    if (children?.length) {
+      menu.children = fn(children, paths);
+    } else {
+      const path = '/' + paths.join('/');
+      menu.label = <Link to={path}>{label}</Link>;
+      paths.pop();
+    }
+
+    return menu;
+  });
+
+  return fn(permissions);
+};
+
 function SiderBar() {
+  const [menus, setMenus] = useState([]);
+  const [defaultSelectedKeys, setDefaultSelectedKeys] = useState([]);
+  const [defaultOpenKeys, setDefaultOpenKeys] = useState([]);
+  const { user } = useSelector(state => state.login);
   const { collapsed } = useSelector(state => state.style);
+  const userPermissions = user?.role.permissions;
   const logoText = '全球新闻发布管理系统';
+
+  const init = useCallback(async () => {
+    const res = await getPermissions();
+    const allPermissions = res.data;
+
+    // 判断当前用户的权限
+    const permissions = userPermissions.includes('*')
+      ? allPermissions
+      : allPermissions.filter(permission => userPermissions.includes(permission));
+
+    const { defaultOpenKeys, defaultSelectedKeys } = getKeys(permissions);
+    const menusTree = getAssembleTree(permissions);
+
+    const menus = getMenus(menusTree);
+
+    setDefaultSelectedKeys(defaultSelectedKeys);
+    setDefaultOpenKeys(defaultOpenKeys);
+    setTimeout(() => {
+      setMenus(menus);
+    });
+  }, [userPermissions]);
+
+  useEffect(() => {
+    init();
+  }, [init]);
 
   return (
     <Sider
@@ -48,28 +149,13 @@ function SiderBar() {
           ].join(' ')}>{logoText}</span>
         </NavLink>
       </Tooltip>
-      <Menu
+      {menus.length && <Menu
         theme="dark"
         mode="inline"
-        defaultSelectedKeys={['1']}
-        items={[
-          {
-            key: '1',
-            icon: <UserOutlined />,
-            label: 'nav 1',
-          },
-          {
-            key: '2',
-            icon: <VideoCameraOutlined />,
-            label: 'nav 2',
-          },
-          {
-            key: '3',
-            icon: <UploadOutlined />,
-            label: 'nav 3'
-          },
-        ]}
-      />
+        defaultOpenKeys={defaultOpenKeys}
+        defaultSelectedKeys={defaultSelectedKeys}
+        items={menus}
+      />}
     </Sider>
   );
 }
---
+++ src/utils/index.js
@@ -0,0 +1,34 @@
+/**
+ * 组装扁平化树形数据
+ * @param {object[]} data 数据源
+ * @param {string} [id="id"] id 字段名
+ * @param {string} [parentId="parentId"] 父节点字段名
+ * @param {string} [children="children"] 子节点字段名
+ * @param {number} [rootId=0] 根 id
+ * @returns {object[]} 组装后的树形数据
+ */
+export function getAssembleTree(
+  data,
+  id = 'id',
+  parentId = 'parentId',
+  children = 'children',
+  rootId = 0
+) {
+  data = Array.isArray(data) ? data : [];
+  // 对源数据深度克隆
+  const cloneData = JSON.parse(JSON.stringify(data));
+  // 循环所有项
+  const treeData = cloneData.filter(father => {
+    // 返回每一项的子级数组
+    const branchArr = cloneData.filter(child => father[id] === child[parentId]);
+
+    if (branchArr.length) {
+      father[children] = branchArr;
+    }
+
+    // 返回第一层
+    return father[parentId] === rootId;
+  });
+
+  return treeData?.length ? treeData : data
+}
