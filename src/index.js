3e0588bf1ca84be8f61684ede348295a303632b8
Daazzer <w513765485@163.com>
2022-10-24 00:37:50 +0800

    修复样式

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './store';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
