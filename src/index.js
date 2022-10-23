e17671154c0319ef9b5e58f272334ba7185e8992
Daazzer <w513765485@163.com>
2022-10-24 00:21:30 +0800

    修改顺序

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import './index.scss';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
