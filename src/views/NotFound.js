ecbf2293c6666ed8ae86f1279473453c3931b5a2
Daazzer <w513765485@163.com>
2022-10-26 16:40:14 +0800

    样式话 404 页

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,6 +1,34 @@
+import logo from '@/assets/logo.svg';
+
 function NotFound() {
   return (
-    <div>404NotFound!</div>
+    <div
+      className="not-found"
+      style={{
+        position: 'relative',
+        height: '100%'
+      }}>
+      <div
+        className="not-found__logo"
+        style={{
+          position: 'absolute',
+          left: '50%',
+          top: '10%',
+          transform: 'translateX(-50%)',
+          display: 'flex',
+          flexFlow: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <img
+          className="not-found__logo__img"
+          width={600}
+          src={logo}
+        />
+        <h2 className="not-found__logo__text">404NotFound!</h2>
+      </div>
+    </div>
   );
 }
 
